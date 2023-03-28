aca818283156c4eeb79d2fe7d1d2dbfc8c70c7fc
rromulo <rromulo.silva21@gmail.com>
2023-03-28 17:46:45 -0300

    chore: create migrations and model for sales

--- back-end/src/database/migrations/20230328201911-sales.js
+++ back-end/src/database/migrations/20230328201911-sales.js
@@ -1,21 +1,30 @@
-'use strict';
+const sale = (Sequelize) => ({
+  id: Sequelize.INTEGER,
+  userId: { type: Sequelize.INTEGER,
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+    references: { model: 'users', key: 'id' },
+    field: 'user_id',
+  },
+  sellerId: { type: Sequelize.INTEGER,
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+    references: { model: 'users', key: 'id' },
+    field: 'seller_id',
+  },
+  totalPrice: { type: Sequelize.DECIMAL, allowNull: false, field: 'total_price' },
+  deliveryAddress: { type: Sequelize.STRING, allowNull: false, field: 'delivery_address' },
+  deliveryNumber: { type: Sequelize.STRING, allowNull: false, field: 'delivery_number' },
+  saleDate: { type: Sequelize.DATE, allowNull: false },
+  status: { type: Sequelize.STRING, allowNull: false },
+});
 
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('sales', sale(Sequelize));
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+  async down(queryInterface, _Sequelize) {
+    await queryInterface.dropTable('sales');
+  },
 };
---
+++ back-end/src/database/models/Sale.js
@@ -0,0 +1,25 @@
+const sale = (DataTypes) => ({
+  id: DataTypes.INTEGER,
+  userId: { type: DataTypes.INTEGER, allowNull: false, foreignKey: true },
+  sellerId: { type: DataTypes.INTEGER, allowNull: false, foreignKey: true },
+  totalPrice: { type: DataTypes.DECIMAL, allowNull: false },
+  deliveryAddress: { type: DataTypes.STRING, allowNull: false },
+  deliveryNumber: { type: DataTypes.STRING, allowNull: false },
+  saleDate: { type: DataTypes.DATE, allowNull: false },
+  status: { type: DataTypes.STRING, allowNull: false },
+});
+
+module.exports = (sequelize, DataTypes) => {
+  const Sale = sequelize.define(
+    'Sale',
+    sale(DataTypes),
+    { underscored: true, timestamps: false, tableName: 'sales' },
+  );
+  Sale.associate = ({ User }) => {
+    Sale.belongsToMany(User, { foreignKey: 'user_id', as: 'user' });
+  };
+  Sale.associate = ({ User }) => {
+    Sale.belongsToMany(User, { foreignKey: 'seller_id', as: 'seller' });
+  };
+  return Sale;
+};
