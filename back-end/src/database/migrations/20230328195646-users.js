933e8e16464ff78eb5f72f5089ab57b5f3f31da8
rromulo <rromulo.silva21@gmail.com>
2023-03-28 17:19:59 -0300

    chore: create migrations and model for users

---
+++ back-end/src/database/migrations/20230328195646-users.js
@@ -0,0 +1,20 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+      },
+      name: { type: Sequelize.STRING, allowNull: false },
+      email: { type: Sequelize.STRING, allowNull: false },
+      password: { type: Sequelize.STRING, allowNull: false },
+      role: { type: Sequelize.STRING, allowNull: false },
+    });
+  },
+
+  async down(queryInterface, _Sequelize) {
+    await queryInterface.dropTable('users');
+  },
+};
---
+++ back-end/src/database/models/User.js
@@ -0,0 +1,17 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: { type: DataTypes.STRING, allowNull: false },
+    email: { type: DataTypes.STRING, allowNull: false },
+    password: { type: DataTypes.STRING, allowNull: false },
+    role: { type: DataTypes.STRING, allowNull: false },
+  }, { underscored: true, timestamps: false, tableName: 'users' });
+  User.associate = ({ Sale }) => { User.hasMany(Sale, { foreignKey: 'user_id', as: 'user' }); };
+  User.associate = ({ Sale }) => { User.hasMany(Sale, { foreignKey: 'seller_id', as: 'seller' }); };
+  return User;
+};
