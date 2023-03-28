0af3b81156dd24ef513e911ccdf2005762fa5c17
caiobacode <caioodsf@gmail.com>
2023-03-28 16:21:26 -0300

    feat: desenvolvendo tela de login req 1, 2

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
+import Login from './pages/Login';
 
 function App() {
   return (
     <div className="App">
-      <span className="logo">TRYBE</span>
-      <object className="rocksGlass" type="image/svg+xml" data={ rockGlass }>
-        Glass
-      </object>
+      <Switch>
+        <Route exact path="/"><Redirect to="/login" /></Route>
+        <Route path="/login" component={ Login } />
+      </Switch>
     </div>
   );
 }
