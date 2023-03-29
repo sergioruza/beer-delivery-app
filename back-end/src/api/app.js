9fcc403d4e1d58da354f6ff6f760d31985d0c4cf
Sergio <sergioruza22@gmail.com>
2023-03-29 14:10:48 -0300

    fix: lint

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,10 +1,9 @@
 const express = require('express');
 require('express-async-errors');
+const cors = require('cors');
 const ErrorHandler = require('./middlewares/errorHandler');
 
 const routes = require('./routes');
-
-const cors = require('cors');
 
 const app = express();
 
@@ -14,6 +13,5 @@ app.use(express.json());
 app.get('/coffee', (_req, res) => res.status(418).end());
 app.use('/login', routes.loginRoute);
 app.use(ErrorHandler.handle);
-
 
 module.exports = app;
