367c4a23b6f0953a387a6843ecfcee7d19a7a1d6
rromulo <rromulo.silva21@gmail.com>
2023-03-30 00:48:29 -0300

    fix: corrige importações e adiciona index nos componentes

--- front-end/src/pages/Products.js
+++ front-end/src/pages/Products.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
-import { Header } from '../components/Header';
+import { Header } from '../components';
 import getLocalStorage from '../services/getLocalStorage';
 
 export default class Products extends Component {
