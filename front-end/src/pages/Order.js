e5c2eff4b383d349cd4c6dfae5236144e07981be
rromulo <rromulo.silva21@gmail.com>
2023-04-01 19:35:10 -0300

    feat: cria verificações para seller orders

--- front-end/src/pages/Order.js
+++ front-end/src/pages/Order.js
@@ -4,25 +4,27 @@ import { OrderDetails, Header } from '../components';
 import { getSalesByUserId } from '../services/requests';
 import getLocalStorage from '../services/getLocalStorage';
 
+const LASTITEM = 1;
 export default class Order extends Component {
   state = {
     order: {},
     renderDetails: false,
+    userType: '',
   };
 
   async componentDidMount() {
     const { history } = this.props;
-    const type = history.location.pathname.split('/')[3];
+    const pathName = history.location.pathname.split('/');
     const userId = getLocalStorage('user', { id: 3 }).id;
     const newOrders = await getSalesByUserId(userId);
-    const order = newOrders.find((o) => o.id === Number(type));
-    this.setState({ order, renderDetails: true });
+    const order = newOrders.find((o) => o.id === Number(pathName.at(-LASTITEM)));
+    this.setState({ order, renderDetails: true, userType: pathName.at(2) });
   }
 
   render() {
     const { history } = this.props;
-    const { order, renderDetails } = this.state;
-    const ROUTE = 'customer_order_details__';
+    const { order, renderDetails, userType } = this.state;
+    const ROUTE = `${userType}_order_details__`;
     const formatedDate = new Date(order.saleDate).toLocaleDateString('pt-BR');
     const ELEMENT_DETAILS = 'element-order-details-label-';
     return (
@@ -33,25 +35,47 @@ export default class Order extends Component {
             <span data-testid={ `${ROUTE}${ELEMENT_DETAILS}order-id` }>
               {order.id}
             </span>
-            <div>
-              P. Vend:
-              <span data-testid={ `${ROUTE}${ELEMENT_DETAILS}seller-name` }>
-                {order.sellerName}
-              </span>
-            </div>
+            {userType === 'customer' && (
+              <div>
+                P. Vend:
+                <span data-testid={ `${ROUTE}${ELEMENT_DETAILS}seller-name` }>
+                  {order.sellerName}
+                </span>
+              </div>
+            )}
             <span data-testid={ `${ROUTE}${ELEMENT_DETAILS}order-date` }>
-              { formatedDate }
+              {formatedDate}
             </span>
             <span data-testid={ `${ROUTE}${ELEMENT_DETAILS}delivery-status` }>
               {order.status}
             </span>
-            <button
-              type="button"
-              disabled={ order.status !== 'Em Trânsito' }
-              data-testid={ `${ROUTE}button-delivery-check` }
-            >
-              {order.status !== 'Entregue' ? 'Marcar como entregue' : 'Ja foi entregue'}
-            </button>
+            {userType === 'customer' && (
+
+              <button
+                type="button"
+                disabled={ order.status !== 'Em Trânsito' }
+                data-testid={ `${ROUTE}button-delivery-check` }
+              >
+                {order.status !== 'Entregue' ? 'Marcar como entregue' : 'Ja foi entregue'}
+              </button>
+            )}
+            {userType === 'seller' && (
+              <>
+                <button
+                  type="button"
+                  data-testid="seller_order_details__button-preparing-check"
+                >
+                  PREPARAR PEDIDO
+                </button>
+
+                <button
+                  type="button"
+                  data-testid="seller_order_details__button-dispatch-check"
+                >
+                  SAIU PARA ENTREGA
+                </button>
+              </>
+            )}
           </div>
         </div>
         {
