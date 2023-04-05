c0d8e2cbbe85671ac497d64ac4b0c1c841372139
Sergio <sergioruza22@gmail.com>
2023-04-05 10:17:55 -0300

    feat: css /register

--- front-end/src/pages/Register.js
+++ front-end/src/pages/Register.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Button from '@mui/material/Button';
+import { Box } from '@mui/material';
+import TextField from '@mui/material/TextField';
 import { loginAPI } from '../services/requests';
 import validateFields from '../utils/validateFields';
 import setLocalStorage from '../services/setLocalStorage';
+import '../css/Register.css';
+import logo from '../images/logo.png';
 
 class Register extends React.Component {
   state = {
@@ -41,34 +46,47 @@ class Register extends React.Component {
       showMsg, disableLoginBtn } = this.state;
 
     return (
-      <div>
-        <h1>Registro</h1>
-        <input
-          type="text"
-          name="name"
-          value={ name }
-          placeholder="nome"
-          onChange={ this.handleChange }
-          data-testid="common_register__input-name"
-        />
-        <input
-          type="email"
-          name="email"
-          required
-          value={ email }
-          placeholder="email"
-          onChange={ this.handleChange }
-          data-testid="common_register__input-email"
-        />
-        <input
-          type="password"
-          name="password"
-          placeholder="senha"
-          value={ password }
-          onChange={ this.handleChange }
-          data-testid="common_register__input-password"
-        />
-        <button
+      <div className="all-register">
+        <h1>Cadastre-se</h1>
+        <img width={ 200 } src={ logo } alt="logo" />
+        <Box
+          className="inputs-register"
+          component="form"
+          sx={ {
+            '& > :not(style)': { m: 1, width: '25ch' },
+          } }
+          autoComplete="off"
+        >
+
+          <TextField
+            type="text"
+            name="name"
+            value={ name }
+            placeholder="nome"
+            onChange={ this.handleChange }
+            data-testid="common_register__input-name"
+          />
+          <TextField
+            type="email"
+            name="email"
+            required
+            value={ email }
+            placeholder="email"
+            onChange={ this.handleChange }
+            data-testid="common_register__input-email"
+          />
+          <TextField
+            type="password"
+            name="password"
+            placeholder="senha"
+            value={ password }
+            onChange={ this.handleChange }
+            data-testid="common_register__input-password"
+          />
+        </Box>
+        <Button
+          className="btn-register"
+          variant="contained"
           type="submit"
           value="Cadastrar"
           disabled={ disableLoginBtn }
@@ -77,16 +95,20 @@ class Register extends React.Component {
         >
           Cadastrar
 
-        </button>
-        {
-          showMsg && (
-            <span
-              data-testid="common_register__element-invalid_register"
-            >
-              {message}
-            </span>
-          )
-        }
+        </Button>
+        <div className="msg-register-div">
+
+          {
+            showMsg && (
+              <span
+                className="msg-register"
+                data-testid="common_register__element-invalid_register"
+              >
+                {message}
+              </span>
+            )
+          }
+        </div>
       </div>
     );
   }
