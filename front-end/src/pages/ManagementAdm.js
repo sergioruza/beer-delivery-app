b18e26ec6fd285f54aca53898a72f923a3618a9f
Sergio <sergioruza22@gmail.com>
2023-04-06 11:31:31 -0300

    feat: validações para preenchimento de campos

--- front-end/src/pages/ManagementAdm.js
+++ front-end/src/pages/ManagementAdm.js
@@ -2,10 +2,35 @@ import React, { Component } from 'react';
 import NavBarAdm from '../components/NavBarAdm';
 import usersMock from '../utils/mocks/usersMock';
 import TableUsers from '../components/tableUsers';
+import validateFields from '../utils/validateFields';
 
 export default class ManagementAdm extends Component {
+  state = {
+    roleSelect: 'customer',
+    msg: '',
+    btnDisable: true,
+    inputPass: '',
+    inputName: '',
+    inputEmail: '',
+  };
+
+  validate = () => {
+    const { inputName, inputEmail, inputPass } = this.state;
+    const result = validateFields(inputEmail, inputPass, inputName);
+    if (result === false) {
+      return this.setState({ btnDisable: false });
+    }
+
+    this.setState({ msg: result });
+  };
+
+  handleChange = ({ target: { name, value } }) => {
+    this.setState({ [name]: value }, () => this.validate());
+  };
+
   render() {
     const ADMIN_MANAGE = 'admin_manage__';
+    const { inputName, inputEmail, inputPass, roleSelect, msg, btnDisable } = this.state;
     return (
       <div>
         <NavBarAdm />
@@ -17,6 +42,9 @@ export default class ManagementAdm extends Component {
             <span>
               Nome
               <input
+                value={ inputName }
+                onChange={ this.handleChange }
+                name="inputName"
                 data-testid={ `${ADMIN_MANAGE}input-name` }
                 placeholder="Nome e sobrenome"
               />
@@ -25,6 +53,9 @@ export default class ManagementAdm extends Component {
             <span>
               Email
               <input
+                value={ inputEmail }
+                onChange={ this.handleChange }
+                name="inputEmail"
                 data-testid={ `${ADMIN_MANAGE}input-email` }
                 placeholder="email@email.com"
               />
@@ -33,19 +64,28 @@ export default class ManagementAdm extends Component {
             <span>
               Senha
               <input
+                value={ inputPass }
+                onChange={ this.handleChange }
+                name="inputPass"
                 type="password"
                 data-testid={ `${ADMIN_MANAGE}input-password` }
                 placeholder="*******"
               />
             </span>
 
-            <select data-testid={ `${ADMIN_MANAGE}select-role` }>
-              <option value="seller">Vendedor</option>
+            <select
+              value={ roleSelect }
+              onChange={ this.handleChange }
+              name="roleSelect"
+              data-testid={ `${ADMIN_MANAGE}select-role` }
+            >
               <option value="customer">Cliente</option>
+              <option value="seller">Vendedor</option>
             </select>
 
             <button
               type="button"
+              disabled={ btnDisable }
               data-testid={ `${ADMIN_MANAGE}button-register` }
             >
               Cadastrar
