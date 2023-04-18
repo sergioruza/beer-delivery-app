18127345851b4b5ce4a9ba799b44f86bae72dfa5
Sergio <sergioruza22@gmail.com>
2023-04-18 10:26:49 -0300

    deploy

--- back-end/src/database/config/config.js
+++ back-end/src/database/config/config.js
@@ -1,22 +1,21 @@
 require('dotenv').config();
 
-const environment = process.env.NODE_ENV || "test";
+const environment = process.env.NODE_ENV || 'test';
 
 const suffix = {
-  prod: "",
-  production: "",
-  dev: "-dev",
-  development: "-dev",
-  test: "-test",
+  prod: '',
+  production: '',
+  dev: '-dev',
+  development: '-dev',
+  test: '-test',
 };
 
 const options = {
-  host: process.env.HOSTNAME || process.env.MYSQL_HOST || 'localhost',
-  port: process.env.MYSQL_PORT || '3306',
-  database: 
-    `${process.env.MYSQL_DB_NAME || 'delivery-app'}${suffix[environment] || suffix.test}`,
-  username: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || 'password',
+  host: process.env.HOSTNAME || process.env.MYSQLHOST || 'localhost',
+  port: process.env.MYSQLPORT || '3306',
+  database: process.env.MYSQLDATABASE || 'delivery-app',
+  username: process.env.MYSQLUSER || 'root',
+  password: process.env.MYSQLPASSWORD || 'password',
   dialect: 'mysql',
   dialectOptions: {
     timezone: 'Z',
